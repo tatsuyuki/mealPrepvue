cdd146e3da771e5cee7cd097a68cd3604ea28ffd
tatsuyuki <michael84@gmail.com>
2019-01-27 21:08:29 +0800

    -first commit

---
+++ src/components/HomeDetails.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-container>
+        <v-layout column>
+            <v-flex class="display-2 text-xs-center my-5">Big Title Goes Here</v-flex>
+            <v-flex>
+                <div class="headline mt-3">Lorem ipsum</div>
+                <p class="subheading mt-3">Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit. Sed vehicula laoreet urna. Donec volutpat, ante 
+                    eu lacinia dictum, nunc dui accumsan urna, non maximus neque tellus 
+                    vitae urna. Sed a consequat nulla. Fusce viverra dui sit amet massa 
+                    elementum molestie. Fusce dapibus dolor sit amet arcu elementum sodales. 
+                    Donec quis elit lorem. Sed pulvinar arcu eget fringilla viverra. Nam nec 
+                    metus luctus, facilisis turpis id, placerat dui. Nulla ac lectus sit amet 
+                    sapien efficitur blandit. Aliquam malesuada maximus augue. Praesent commodo 
+                    facilisis ultricies. Quisque velit ligula, ultricies quis magna et, tristique 
+                    blandit eros. Praesent a semper tortor. Maecenas quis lectus maximus, bibendum 
+                    libero ac, vestibulum felis. Mauris vitae sodales mauris. Aenean elementum 
+                    dignissim varius.</p>
+            </v-flex>
+            <v-flex>
+                <div class="headline mt-3">Lorem ipsum</div>
+                <p class="subheading mt-3">Duis efficitur est ut cursus tincidunt. Ut vitae lorem 
+                    nisi. Donec blandit mi in bibendum tempor. Nam a est at sapien luctus gravida. 
+                    In ut hendrerit odio. Maecenas viverra felis lorem, at fermentum justo tempus 
+                    rutrum. Proin dapibus, leo vel efficitur imperdiet, urna magna maximus tortor, 
+                    vitae bibendum augue orci in tellus. Cras scelerisque gravida lorem, eget 
+                    pulvinar nunc tempus quis. Nam eget metus neque. Suspendisse iaculis elit non 
+                    sagittis rutrum. Aenean dictum pretium libero, vitae pharetra sem consequat ac. 
+                    Pellentesque congue congue eros eu ultricies. Aenean in augue turpis. Nulla nunc 
+                    diam, ornare a fermentum ac, luctus id ante.</p>
+
+                <p class="subheading mt-3">Aenean posuere, arcu vel aliquam malesuada, felis lorem mollis urna, ut rutrum nisl erat in velit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vel iaculis mi. Nam ac imperdiet elit, posuere commodo velit. Nulla euismod posuere dignissim. Suspendisse interdum vel leo non maximus. Etiam a purus sit amet eros pharetra ultrices nec at neque. Sed in sem non ipsum imperdiet porta eu vel felis. Mauris vulputate faucibus ipsum, non fermentum velit vulputate eget. Duis vel pretium sapien, a porttitor nisl. Maecenas sodales tortor ornare convallis scelerisque. Aenean erat massa, feugiat ut faucibus ac, blandit pellentesque urna. Sed ultrices dapibus lectus non auctor. Vestibulum massa tortor, scelerisque et accumsan in, aliquet a diam. Praesent eget gravida magna. Cras ac ligula risus.</p>
+
+                <p class="subheading mt-3">Nunc maximus tincidunt erat, eu fermentum massa hendrerit eu. Nunc purus ipsum, pellentesque in dui vitae, tristique pulvinar sem. Praesent convallis tellus nulla, a pellentesque arcu pellentesque id. Maecenas vel mi id tellus condimentum fermentum. In porttitor sollicitudin metus, non malesuada magna placerat eget. Vestibulum quis nisi et diam facilisis euismod. Aliquam molestie volutpat orci. Maecenas nisl ipsum, aliquet sit amet varius quis, ornare a risus.</p>
+            </v-flex>
+            <v-flex>
+                <div class="headline mt-3">Lorem ipsum</div>
+                <p class="subheading mt-3">Mauris eu tortor quis felis porttitor gravida vitae viverra magna. Maecenas feugiat venenatis lacus, id volutpat lorem ornare maximus. Nullam lorem tortor, aliquam eget justo eget, consectetur lacinia magna. Vivamus magna massa, laoreet ac tortor eu, vulputate luctus ipsum. Praesent suscipit nec urna sit amet tempor. Nullam vitae erat pellentesque, venenatis lectus et, posuere turpis. Integer a mi justo. Quisque eget nisi vel lacus placerat pulvinar. Fusce sem enim, feugiat vel enim at, mattis condimentum lacus. Nullam sit amet interdum eros. Nam ac malesuada mauris, sed ultricies risus.</p>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomeDetails'
+};
+</script>
+
+<style scoped>
+
+</style>
+
+
--- src/store.js
+++ src/store.js
@@ -1,10 +1,36 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import axios from 'axios';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    recipes: [],
+    apiURL: 'https://api.edamam.com/search'
+  },
+  mutations: {
+    setRecipes(state, payload) {
+      state.recipes = payload;
+    },
+  },
+  actions: {
+    async getRecipes({ state, commit }, plan) {
+      try {
+        let response = await axios.get(`${state.apiURL}`, {
+          params: {
+            q: plan,
+            app_id: '93e63bc7',
+            app_key: '8bac168c4cf65ac80926cfe6bc69c9b4',
+            from: 0,
+            to: 9
+          }
+        });
+        commit('setRecipes', response.data.hits);
+      } catch (error) {
+        commit('setRecipes', []);
+      }      
+    }
+  }
 });
